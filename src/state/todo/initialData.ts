fd2c063891cfc9784153bef33d380c09e7961b23
Maksym Holovanov <maksym.holovanov@check24.de>
2026-05-19 14:03:00 +0200

    Remove init data

--- src/state/todo/initialData.ts
+++
@@ -1,9 +0,0 @@
-import type { Todo } from "./types";
-
-export const INITIAL_TODOS: Todo[] = [
-  { id: 1, title: "buy groceries1", status: "Pending" },
-  { id: 2, title: "go to the gym1", status: "Pending" },
-  { id: 3, title: "read a book1", status: "Pending" },
-  { id: 4, title: "watch a movie1", status: "Pending" },
-  { id: 5, title: "go for a walk1", status: "Pending" },
-];

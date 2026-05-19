d86b39d9f1a55c5e627c6da063ad9c6b4da72920
Maksym Holovanov <maksym.holovanov@check24.de>
2026-05-19 11:11:07 +0200

    remove useTodoStateHandler

--- src/state/todo/useTodoActions.ts
+++ src/state/todo/useTodoActions.ts
@@ -1,10 +1,11 @@
 import { useSetAtom } from "jotai";
-import { addTodoAtom, removeTodoAtom, toggleTodoAtom } from "./actions";
+import { addTodoAtom, removeTodoAtom, toggleTodoAtom, editTodoAtom } from "./actions";
 import type { TodoActions } from "./types";
 
 export const useTodoActions = (): TodoActions => {
   const addTodo = useSetAtom(addTodoAtom);
   const toggleTodo = useSetAtom(toggleTodoAtom);
   const removeTodo = useSetAtom(removeTodoAtom);
-  return { addTodo, toggleTodo, removeTodo };
+  const editTodo = useSetAtom(editTodoAtom);
+  return { addTodo, toggleTodo, removeTodo, editTodo };
 };

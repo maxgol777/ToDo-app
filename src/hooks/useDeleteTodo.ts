b15705dfb2d32270bf5820ebb0a6661b474584d1
Maksym Holovanov <maksym.holovanov@check24.de>
2026-05-19 17:52:48 +0200

    update import

--- src/hooks/useDeleteTodo.ts
+++ src/hooks/useDeleteTodo.ts
@@ -1,7 +1,6 @@
 import { useSetAtom } from "jotai";
 import { todosAtom } from "../state/todo/atoms";
-import { deleteTodo } from "../services/todo/todoApi.ts";
-import { fetchTodos } from "../services/todo/todoApi.ts";
+import { deleteTodo, fetchTodos } from "../services/todo/todoApi.ts";
 
 export const useDeleteTodo = () => {
   const setTodos = useSetAtom(todosAtom);

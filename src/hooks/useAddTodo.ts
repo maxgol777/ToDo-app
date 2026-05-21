0f827c0d8ba14b303051418516f5d9630b69d535
Maksym Holovanov <maksym.holovanov@check24.de>
2026-05-21 13:28:40 +0200

    add artificial delay in useAddTodo

--- src/hooks/useAddTodo.ts
+++ src/hooks/useAddTodo.ts
@@ -2,11 +2,17 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { addTodo } from "../services/todo/todoApi.ts";
 import { todoQueryKeys } from "../services/todo/queryKeys.ts";
 
+const ARTIFICIAL_DELAY_MS = 1000;
+
 export const useAddTodo = () => {
   const queryClient = useQueryClient();
 
   const { mutateAsync, isPending } = useMutation({
-    mutationFn: (title: string) => addTodo(title),
+    mutationFn: async (title: string) => {
+      // Added for test purpose
+      await new Promise((resolve) => setTimeout(resolve, ARTIFICIAL_DELAY_MS));
+      return addTodo(title);
+    },
     onSuccess: () => {
       void queryClient.invalidateQueries({ queryKey: todoQueryKeys });
     },
